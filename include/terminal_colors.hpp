6ba58549873815db81a8be1609932ff4498d946b
448L <sendel.lucario@gmail.com>
2023-03-29 20:12:23 -0400

    Avanzando con la configuracion SwapChain ya esta creado Y ademas añadir unas cuantas extensiones, para otra ocasion

---
+++ include/terminal_colors.hpp
@@ -0,0 +1,11 @@
+// Foreground
+#define FindianRed1 "\033[38;5;203m"
+#define FRojo3 "\033[38;5;160m"
+#define FSteelBlue1 "\033[38;5;75m"
+
+// Background
+#define BLightGoldenRod1 "\033[38;5;227m"
+
+// Otros
+#define Bold "\033[1m"
+#define Reset "\033[0m"
--- src/Vulkan/Setup_Vulkan.cpp
+++ src/Vulkan/Setup_Vulkan.cpp
@@ -1,16 +1,17 @@
 #include "Graphics/Vulkan.hpp"
 #include "Core/terminal_colors.hpp"
 
-// Instancia
 void Graphics::VulkanRenderer::CreateInstance(SDL_Window * WINDOW) {
-	// Obtener las extensiones
+	// Get Extensions
 	unsigned int extensionInstanceCount = 0;
 	if (!SDL_Vulkan_GetInstanceExtensions(WINDOW, &extensionInstanceCount, nullptr))
-		std::cout << "X Error al obtener la lista de extensiones necesarias: " << SDL_GetError() << std::endl;
+		std::cout << "X Error al obtener la lista de extensiones necesarias : " << SDL_GetError() << std::endl;
 
 	std::vector<const char*> extensionInstanceNames(extensionInstanceCount);
+	extensionInstanceNames.push_back("VK_KHR_get_physical_device_properties2");
+
 	if (!SDL_Vulkan_GetInstanceExtensions(WINDOW, &extensionInstanceCount, extensionInstanceNames.data()))
-		std::cout << "X Error al obtener la lista de extensiones necesarias: " << SDL_GetError() << std::endl;
+		std::cout << "X Error al obtener la lista de extensiones necesarias : " << SDL_GetError() << std::endl;
 	else {
 		std::cout << FRojo3 << Bold << "+ Vulkan Instance Extensions :" << Reset << std::endl;
 		for (int c = 0; c < int(extensionInstanceNames.size()); c++)
@@ -18,17 +19,15 @@ void Graphics::VulkanRenderer::CreateInstance(SDL_Window * WINDOW) {
 		std::cout << Reset << std::endl;
 	}
 
-	// Informacion de la aplicacion
 	vk::ApplicationInfo PatataEngineInfo(
 		"Patata Engine",
 		VK_MAKE_VERSION(1,0,0),
 		"Patata Engine",
 		VK_MAKE_VERSION(1,0,0),
-		VK_API_VERSION_1_2);
+		VK_API_VERSION_1_3);
 
 	std::vector <vk::ExtensionProperties> extensiones = vk::enumerateInstanceExtensionProperties();
 
-	// Instancia
 	vk::InstanceCreateInfo VulkanInstanceInfo(
 		vk::InstanceCreateFlags(),
 		&PatataEngineInfo, layer, nullptr);
@@ -38,7 +37,6 @@ void Graphics::VulkanRenderer::CreateInstance(SDL_Window * WINDOW) {
 	VulkanInstance = vk::createInstance(VulkanInstanceInfo);
 }
 
-// Enumerar los dispositivos fisicos
 void Graphics::VulkanRenderer::PhysicalDevices(void) {
 	PhysicalDevice = VulkanInstance.enumeratePhysicalDevices().front();
 	PhysicalDeviceProperties = PhysicalDevice.getProperties();
@@ -72,9 +70,30 @@ uint32_t Graphics::VulkanRenderer::CreateQueue(void) {
 			GraphicsQueueFamilyIndex = i;
 			break;
 		}
-
-	std::cout << " Compute Queue Family Index:\t" << GraphicsQueueFamilyIndex << std::endl << std::endl;
 	return GraphicsQueueFamilyIndex;
+}
+
+void Graphics::VulkanRenderer::InitDevice(void) {
+	vk::DeviceQueueCreateInfo DeviceQueueCreateInfo(
+			vk::DeviceQueueCreateFlags(),
+			uint32_t(GraphicsQueueFamilyIndex), 1,
+			&QueuePriority);
+
+	vk::DeviceCreateInfo DeviceCreateInfo;
+	DeviceCreateInfo.enabledExtensionCount = uint32_t(DeviceExtensions.size());
+	DeviceCreateInfo.ppEnabledExtensionNames = DeviceExtensions.data();
+	DeviceCreateInfo.pNext = {};
+	DeviceCreateInfo.queueCreateInfoCount = 1;
+	DeviceCreateInfo.pQueueCreateInfos = &DeviceQueueCreateInfo;
+
+	if (DeviceExtensions.size() >= 1) {
+		std::cout << Bold << FindianRed1 << "+ Vulkan Device Extensions :" << Reset << std::endl;
+		for (auto Extensions : DeviceExtensions)
+			std::cout << Bold << FindianRed1 << "| · " << Reset << Extensions << Reset << std::endl;
+		std::cout << std::endl;
+	}
+
+	Device = PhysicalDevice.createDevice(DeviceCreateInfo);
 }
 
 void Graphics::VulkanRenderer::CreateSurface(SDL_Window * WINDOW) {
@@ -85,28 +104,84 @@ void Graphics::VulkanRenderer::CreateSurface(SDL_Window * WINDOW) {
 }
 
 void Graphics::VulkanRenderer::CreateSwapChain(void) {
+	Formats = PhysicalDevice.getSurfaceFormatsKHR(Surface);
+	Format = (Formats[0].format == vk::Format::eUndefined) ? vk::Format::eB8G8R8A8Unorm : Formats[0].format;
 
+	SurfaceCapabilities = PhysicalDevice.getSurfaceCapabilitiesKHR(Surface);
+	PresentModes = PhysicalDevice.getSurfacePresentModesKHR(Surface);
+	SurfaceFormats = PhysicalDevice.getSurfaceFormatsKHR(Surface);
+
+	SwapChainExtent.width = 1280;
+	SwapChainExtent.height = 720;
+
+	for (auto Mode : PresentModes)
+		if (Mode == vk::PresentModeKHR::eFifo)
+			PresentMode = vk::PresentModeKHR::eFifo;
+		else
+			PresentMode = Mode;
+
+	SurfaceFormat = SurfaceFormats[0];
+
+	vk::SwapchainCreateInfoKHR SwapChainCreateInfo;
+	SwapChainCreateInfo.surface = Surface;
+	SwapChainCreateInfo.minImageCount = SurfaceCapabilities.minImageCount;
+	SwapChainCreateInfo.imageFormat = SurfaceFormat.format;
+	SwapChainCreateInfo.imageColorSpace = SurfaceFormat.colorSpace;
+	SwapChainCreateInfo.imageExtent = SwapChainExtent;
+	SwapChainCreateInfo.imageArrayLayers = 1;
+	SwapChainCreateInfo.imageUsage = vk::ImageUsageFlagBits::eColorAttachment;
+	SwapChainCreateInfo.preTransform = SurfaceCapabilities.currentTransform;
+	SwapChainCreateInfo.compositeAlpha = vk::CompositeAlphaFlagBitsKHR::eOpaque;
+	SwapChainCreateInfo.presentMode = PresentMode;
+	SwapChainCreateInfo.clipped = VK_TRUE;
+	SwapChainCreateInfo.queueFamilyIndexCount = GraphicsQueueFamilyIndex;
+	SwapChainCreateInfo.oldSwapchain = nullptr;
+
+	SwapChain = Device.createSwapchainKHR(SwapChainCreateInfo);
+
+	//std::vector<vk::Image> swapchainImages = PhysicalDevice.getSwapchainImagesKHR(SwapChain);
 }
 
 void Graphics::VulkanRenderer::CreateCommandBuffer(void) {
-	CommandPool = Device.createCommandPool(vk::CommandPoolCreateInfo(vk::CommandPoolCreateFlags(), GraphicsQueueFamilyIndex));
-	CommandBuffer = Device.allocateCommandBuffers(vk::CommandBufferAllocateInfo(CommandPool, vk::CommandBufferLevel::ePrimary, 1)).front();
+	/*CommandPool = Device.createCommandPool(vk::CommandPoolCreateInfo(vk::CommandPoolCreateFlags(), GraphicsQueueFamilyIndex));
+	CommandBuffer = Device.allocateCommandBuffers(vk::CommandBufferAllocateInfo(CommandPool, vk::CommandBufferLevel::ePrimary, 1)).front();*/
+}
+
+void Graphics::VulkanRenderer::VulkanInfo(void) {
+	const uint32_t VulkanVersion = PhysicalDeviceProperties.apiVersion;
+
+	std::string vsync;
+	if (PresentMode == vk::PresentModeKHR::eFifo)
+		vsync = "Yes | Using eFifo";
+	else
+		vsync = "No";
+
+	std::cout <<  FindianRed1 << Bold << "+ Informacion general de Vulkan :" << Reset << std::endl;	
+	std::cout << FindianRed1 << Bold << "| · " << Reset << Bold << vk::to_string(PhysicalDeviceProperties.deviceType) << ":\t" << Reset << PhysicalDeviceProperties.deviceName << " " << PhysicalDeviceProperties.vendorID << Reset << std::endl;
+	std::cout << FindianRed1 << Bold << "| · " << Reset << Bold << "Vulkan Version:\t" << Reset << VK_VERSION_MAJOR(VulkanVersion) << '.' << VK_VERSION_MINOR(VulkanVersion) << '.' << VK_VERSION_PATCH(VulkanVersion) << '.' << VK_API_VERSION_VARIANT(VulkanVersion) << Reset << std::endl;
+	std::cout << FindianRed1 << Bold << "| · " << Reset << Bold << "Driver Version:\t" << Reset << PhysicalDeviceProperties.driverVersion << std::endl;
+	std::cout << FindianRed1 << Bold << "| · " << Reset << Bold << "Vsync:\t" << Reset << vsync << std::endl;
+
+	std::cout << std::endl;
 }
 
 Graphics::VulkanRenderer::VulkanRenderer(SDL_Window * WINDOW) {
 	CreateInstance(WINDOW);
 	PhysicalDevices();
-	InitDevice();
 	GraphicsQueueFamilyIndex = CreateQueue();
-	CreateSurface(WINDOW);
+	InitDevice();	
+	CreateSurface(WINDOW);	
 	CreateSwapChain();
 	CreateCommandBuffer();
+	VulkanInfo();
 }
 
 Graphics::VulkanRenderer::~VulkanRenderer(void) {
-	vkDestroySurfaceKHR(VulkanInstance, Surface, nullptr);
-	Device.freeCommandBuffers(CommandPool, CommandBuffer);
-	Device.destroyCommandPool(CommandPool);
+	//Device.destroyCommandPool(CommandPool);
+	//Device.freeCommandBuffers(CommandPool, CommandBuffer);
+
+	Device.destroySwapchainKHR(SwapChain);
+	VulkanInstance.destroySurfaceKHR(Surface);
 	Device.destroy();
 	VulkanInstance.destroy();
 }
