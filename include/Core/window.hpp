627432938cc4176812d04d2af2dd56adeb205a0b
Sendan <sendel.lucario@gmail.com>
2023-05-25 13:45:39 +0000

    Update window.hpp

--- include/Core/window.hpp
+++ include/Core/window.hpp
@@ -12,18 +12,14 @@ namespace Game {
 			~Window();
 
 			// Get
-			SDL_Renderer * Renderer_get(void);
 			SDL_Window * Window_get(void);
 
 			// Set
-			void Renderer_set(SDL_Renderer *);
 			void Window_set(SDL_Window *);
 
 			SDL_GLContext glcontext_get(void);
 
 		private:
 			SDL_Window * WINDOW;
-			SDL_Renderer * Render;
-			SDL_GLContext OPENGL_CONTEXT = NULL;
 	};
 }
