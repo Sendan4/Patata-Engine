4c21f625a40f04bb5cd9806d6c60e12ce70ba418
Sendan <sendel.lucario@gmail.com>
2024-02-02 13:50:50 +0000

    Update Main.cpp

--- examples/Simple Program/Main.cpp
+++ examples/Simple Program/Main.cpp
@@ -3,33 +3,7 @@
 #include <PatataEngine/PatataEngine.hpp>
 
 int main(int argc, char ** argv) {
-	Patata::Engine Patata;
-
-    SDL_Window * window = SDL_CreateWindow(
-		#if defined(DEBUG)
-		"Patata Engine | Debug (Development)",
-		#else
-		"Patata Engine",
-		#endif
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        1280, 720,
-        Patata.GetWindowFlags());
-
-	// Window Icon
-	// Esta es una fuente de errores en linux
-	/*{
-		SDL_Surface * Icon = SDL_LoadBMP(strcat(SDL_GetBasePath(), PATATA_GAME_ICON_FILE));
-		SDL_SetWindowIcon(window ,Icon);
-		SDL_FreeSurface(Icon);
-	}*/
-
-	SDL_GLContext * context = nullptr;
- 
-	Patata.Info->pWindow = window;
-	Patata.Info->pOpenGLContext = context;
-
-	Patata.InitRenderer();
+	Patata::Engine Patata("", 1280, 720);
 
 	bool run = true;
 	while(run) {
@@ -49,6 +23,5 @@ int main(int argc, char ** argv) {
 		Patata.Render();
 	}
 
-	SDL_DestroyWindow(window);
 	return 0;
 }
